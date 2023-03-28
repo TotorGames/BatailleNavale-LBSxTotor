74dc3b55e8aa414c263f0e306e40c16b9230afbb
TotorGames <afropro2006@gmail.com>
2023-03-28 18:59:01 +0200

    1.3

--- index.py
+++ index.py
@@ -6,3 +6,19 @@ for i in range(16):
 
 
 
+# Etape 1 : Généré une matrice rempli de 0
+# Etape 2 : Généré les Différents Bateaux qui sont des liste qui contienne plusieur donnée comme leur sens, leur taille ect..
+# Etape 3 : Vérifier si le bateau est vertical ou horizontal en créant une fonction is_vert
+# Etape 4 : Générer les coordonnés au hasard pour les bateaux.
+# Etape 5 : Changer les valeurs des position des bateaux en 1
+# Etape 6 : Colorier ses cases qui ont pour valeur 1 en noir
+# Etape 7 : Créer une nouvelle matrice 
+# Etape 8 : Généré les Différents Bateaux qui sont des liste qui contienne plusieur donnée comme leur sens, leur taille ect..
+# Etape 9 : Vérifier si le bateau est vertical ou horizontal en créant une fonction is_vert
+# Etape 10 : Générer les coordonnés au hasard pour les bateaux.
+# Etape 11 : Changer les valeurs des position des bateaux en 1
+# Etape 12 : On va demander a chaque tour une coordonnés au joueur adverse
+# Etape 13 : On vérifie si la position est égal à 1 si oui alors la case sera Colorier en rouge  
+# Etape 14 : on créra un variable qui rajoutera 1 a chaque fois que une case sera trouver et si elle est égal a 14  
+
+
